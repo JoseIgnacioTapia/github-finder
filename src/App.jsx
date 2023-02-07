42eb617f0223940aa18209bcd9f22dde7faf270c
JoeIt <josignaciot@gmail.com>
2023-02-07 16:44:59 -0300

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="bg-purple-500">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn">Button</button>
+    </div>
+  );
+}
+
+export default App;
