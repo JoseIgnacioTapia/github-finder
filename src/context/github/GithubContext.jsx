cc4a310feece6ecc3468f10c7f83382ee2b6f1f1
JoeIt <josignaciot@gmail.com>
2023-02-09 18:14:17 -0300

    Reducers & useReducer Hook

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -8,12 +8,15 @@ const GITHUB_URL = import.meta.env.VITE_APP_GITHUB_URL;
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
-    loading: true,
+    loading: false,
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
+  // Get initial users (testing purposes)
   const fetchUsers = async () => {
+    setLoading();
+
     const response = await fetch(`${GITHUB_URL}/users`);
     const data = await response.json();
 
@@ -22,6 +25,9 @@ export const GithubProvider = ({ children }) => {
       payload: data,
     });
   };
+
+  // Set Loading
+  const setLoading = () => dispatch({ type: 'SET_LOADING' });
 
   return (
     <GithubContext.Provider
